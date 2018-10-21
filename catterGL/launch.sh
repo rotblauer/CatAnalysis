eab4c4a0dadb7c1cb2a96991b2519b42c97f98f9
jlanej <jlanej@gmail.com>
2018-10-21 17:28:31 -0500

    :floppy_disk:

--- catterGL/launch.sh
+++ catterGL/launch.sh
@@ -4,5 +4,3 @@
 rsync -avz ./catterStatsStream.html arew:/home/freyabison/catStats.rotblauer.com/index.html
 rsync -avz ./map.png arew:/home/freyabison/catStats.rotblauer.com/map.png
 
-rsync -avz ./getCats.html arew:/home/freyabison/punktlich.rotblauer.com/install/getCats.html
-rsync -avz ./cat.png arew:/home/freyabison/punktlich.rotblauer.com/install/cat.png
