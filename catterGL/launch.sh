92855b65c65377b91facbe7b541980e8c206de58
jlanej <jlanej@gmail.com>
2018-12-02 17:49:16 -0600

    not reasonable

--- catterGL/launch.sh
+++ catterGL/launch.sh
@@ -4,3 +4,5 @@
 rsync -avz ./catterStatsStream.html arew:/home/freyabison/catStats.rotblauer.com/index.html
 rsync -avz ./map.png arew:/home/freyabison/catStats.rotblauer.com/map.png
 
+rsync -avz ./catterGL.html arew:/home/freyabison/catStats.rotblauer.com/catterGL.html
+rsync -avz ./mapCat.png arew:/home/freyabison/catStats.rotblauer.com/mapCat.png
